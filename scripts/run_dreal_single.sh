3640cc6b6aa150e77afe58d9282f94d74dd0efde
Yaoguang Zhai <22529912+yazhai@user.noreply.github.com>
2023-02-23 15:46:58 -0800

    script: update single dreal run

--- scripts/run_dreal_single.sh
+++ scripts/run_dreal_single.sh
@@ -21,13 +21,20 @@ sat_answer=""
 # Timing command
 # echo Running command: $command  # For debugging
 # echo Timing command: $timeout_command  # For debugging
-{ time (eval $timeout_command) ; } >> $out_tmp 2>&1
+
+# Uncomment the test below to avoid re-running the command 
+# if the output file already exists
+#if [ ! -e $out_tmp ] ; then
+    { time (eval $timeout_command) ; } >> $out_tmp 2>&1
+#fi
 
 
 
-# Retrieve time 
-time_elapsed=$(cat $out_tmp | grep real | cut -d 'm' -f 2 | cut -d 's' -f 1)
 
+# Retrieve time
+minutes_elapsed=$(cat $out_tmp | grep real | cut -d 'm' -f 1 | awk '{print($NF)}' )
+seconds_elapsed=$(cat $out_tmp | grep real | cut -d 'm' -f 2 | cut -d 's' -f 1)
+time_elapsed=$(echo "$minutes_elapsed * 60 + $seconds_elapsed" | bc)
 
 # Retrieve answer
 sat_answer=$(cat $out_tmp | grep "delta-sat" | cut -d ' ' -f 1)
